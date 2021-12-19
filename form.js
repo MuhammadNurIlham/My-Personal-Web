f7c9e667ea31f6ef0db2790463525c40dee363d5
Muhammad Nur Ilham <muhnilham99@gmail.com>
2021-12-20 03:51:54 +0700

    First-Push

---
+++ form.js
@@ -0,0 +1,17 @@
+function submitData() {
+    let name = document.getElementById('input-name').value
+    let email = document.getElementById('input-email').value
+    let phone = document.getElementById('input-phone').value
+    let subject = document.getElementById('input-subject').value
+    let message = document.getElementById('input-message').value
+    let checkbox = document.getElementById('checkbox').checked
+
+    if (name == '' || email == '' || phone == '' || subject == '' || message == '' || checkbox == false) {
+        return alert("Semua Form harus diisi yaa brayy...")
+    }
+
+// let emailReceiver = 'muhnilham99@gmail.com'
+let a = document.createElement('a')
+a.href = `mailto:${emailReceiver}?subject=${subject}&body=Halo my name ${name}, ${message}`
+a.click()
+}
